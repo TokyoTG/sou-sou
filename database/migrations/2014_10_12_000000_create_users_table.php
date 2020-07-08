1a20ad62d9b38d313f4b5aa484b3ce095c8651e3
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-07-08 10:23:23 +0100

    setup database tables and controllers

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('full_name');
-            $table->string('position');
-            $table->integer('phone_number');
-            $table->integer('account_number');
-            $table->integer('groups_number');
+            $table->string('current_position');
+            $table->integer('phone_number')->unique();
+            $table->integer('account_number', 30)->unique();
+            $table->integer('groups_in');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
