5dc3faa110ee187347c77a3a2e503b543141fd58
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-07-10 22:44:48 +0100

    add more FE views

--- resources/views/dashboard/wait_list.blade.php
+++ resources/views/dashboard/wait_list.blade.php
@@ -2,36 +2,56 @@
 @section('custom_css')
     
 @endsection
-@section('sidebar')
-<li >
-<a href="{{route('dashboard.index')}}"><i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
-</li>
-<li class="menu-title">Menu</li><!-- /.menu-title -->
-<li >
-    <a href="{{route('users.index')}}"> <i class="menu-icon fa fa-users"></i>Users</a>
-</li>
-    <li>
-    <a href="{{route('groups.index')}}"> <i class="menu-icon fa fa-folder"></i>Groups</a>
-</li>
-    <li class="active">
-    <a href="{{route('dashboard.wait_list')}}"> <i class="menu-icon fa fa-list-alt"></i>Wait List</a>
-</li>
-<li>
-    <a href="{{route('dashboard.complaints')}}"> <i class="menu-icon fa fa-list-alt"></i>Complaints</a>
-</li>
-<li>
-    <a href="{{route('dashboard.settings')}}"> <i class="menu-icon fa fa-cogs"></i>Settings</a>
-</li>
-@endsection
 
 
 
 @section('title')
-    <h1>Wait List</h1>
+    <h4>Wait List</h4>
 @endsection
 @section('contents')
-  <div class="content">
-            <div class="animated fadeIn">
+
+  <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011/04/25</td>
+                      <td>$320,800</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+</div>
+            {{-- <div class="animated fadeIn">
                 <div class="row">
 
                     <div class="col-md-12">
@@ -64,8 +84,7 @@
 
 
                 </div>
-            </div><!-- .animated -->
-        </div>
+            </div> --}}
 @endsection
 
 @section('custom_js')
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,7 +12,7 @@
             <div class="text-center">
               <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
             </div>
-            <form class="user">
+          <form class="user" action="{{route('register')}}">
               <div class="form-group row">
                 <div class="col-sm-6 mb-3 mb-sm-0">
                   <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
@@ -21,8 +21,13 @@
                   <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
                 </div>
               </div>
-              <div class="form-group">
-                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+              <div class="form-group row">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                   <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                </div>
+                <div class="col-sm-6">
+                  <input type="tel" class="form-control form-control-user" id="exampleLastName" placeholder="Phone Number">
+                </div>
               </div>
               <div class="form-group row">
                 <div class="col-sm-6 mb-3 mb-sm-0">
