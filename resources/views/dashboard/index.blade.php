bfc0a34eb97368cf43fc17a933c2709150c08cf8
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-07-19 03:01:21 +0100

    landing page roughly done

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -28,62 +28,72 @@
 
 @if (Cookie::get('role') !== null && Cookie::get('role') == "admin")
     @isset($admin)
-    <div class="row">
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Users</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$admin['users']}}</div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-user fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Groups</div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{$admin['groups']}}</div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-users fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Wait List</div>
+    <div class="content">
+      <div class="animated fadeIn">
+        
+        <div class="col-md-12">
+          <div class="row d-flex justify-content-around">
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
                   <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Users</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{$admin['users']}}</div>
+                    </div>
                     <div class="col-auto">
-                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$admin['list']}}</div>
+                      <i class="fas fa-user fa-2x text-gray-300"></i>
                     </div>
                   </div>
                 </div>
-                <div class="col-auto">
-                  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+              </div>
+            </div>
+        
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Groups</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">{{$admin['groups']}}</div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Wait List</div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$admin['list']}}</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
+       
+      </div>
     </div>
+ 
     @endisset
 @endif
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,286 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
+        <title>Sou Sou | Home</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
+            *{
+                padding: 0;
                 margin: 0;
+                box-sizing: border-box;
+                text-decoration: none;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
+         
+            .top-right{
+                padding: 10px 15px;
+                background: white;
+                font-family: 'Nunito', sans-serif;
                 display: flex;
-                justify-content: center;
+                justify-content: space-around;
             }
 
-            .position-ref {
+            .top-right a{
+                display: inline-block;
+                margin-right: 15px;
+                padding: 10px 15px;
+                transition: .3s;
+                font-size: 1.1em;
+                color: #212121;
+                font-weight: 600;
+            }
+            .top-right h2{
+                margin-right: auto;
+            }
+
+            .top-right  .register{
+                border: 1px solid #4e73df;
+                border-radius: 5px;
+                background: #4e73df;
+                color: #fff ;
+            }
+
+            .links.register:hover{
+                background: #fff;
+                color:#4e73df ;
+            }
+
+            .links:hover{
+                 color: #4e73df;
+                 /* text-decoration: underline; */
+            }
+
+            #menu{
+                display: none;
+            }
+
+            #link-container{
+                width: unset;
+                height: unset;
+                transition: .5s;
+            }
+
+            #close-btn{
+                display: none;
+            }
+            #menu{
+                display: none;
+            }
+
+            #welcome{
+                margin-top: 20px;
                 position: relative;
+                font-family: 'Oswald', sans-serif;
+                background: #4e73df;
+                min-height: 350px;
+                color: #f1f1f1;
             }
 
-            .top-right {
+            #welcome h1{
+                font-size: 3.5em;
+
+            }
+
+            #w-text{
                 position: absolute;
-                right: 10px;
-                top: 18px;
+                top:50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
             }
 
-            .content {
+            #how{
+                text-align: center;
+                font-family: 'Oswald', sans-serif;
+                margin-top: 20px;
+            }
+
+
+
+            .content{
+                width: 100%;
+                padding: 10px 25px;
                 text-align: center;
             }
-
-            .title {
-                font-size: 84px;
+            .cards{
+                display: inline-block;
+                width: 300px;
+                position: relative;
+                padding: 15px 5px 0px 5px;
+                height: 420px;
+                margin: 15px;
+                background: #4e73df;
+                border-radius: 15px;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
+
+            .cards img{
+                width: 100%;
+                height: 300px;
+            }
+
+
+            .cards p{
+                color: #fff;
+                font-size: 1.2em;
+                font-family: 'Nunito', sans-serif;
+                padding: 5px 10px;
                 font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
+                /* position: absolute;
+                top: 10px; */
             }
 
-            .m-b-md {
-                margin-bottom: 30px;
+
+            @media only screen and (max-width: 650px) {
+                /* .top-right .links{
+                  display: none;
+              } */
+              #menu{
+                display: block;
+                cursor: pointer;
             }
+
+         
+            #link-container{
+                    background: #fff;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 0;
+                    padding-top: 20px;
+                    z-index: 100;
+                    height: 100vh;
+                    overflow: hidden;
+                }
+                .top-right a{
+                    display: block;
+                    margin-top: 10px;
+                    width: 50%;
+                }
+
+                .top-right  .register{
+                border: 0px solid #4e73df;
+                border-radius: 5px;
+                background: none;
+                color: #212121 ;
+            }
+            #close-btn{
+                display: block;
+                font-size: 3.5em;
+                font-weight: bold;
+                position: absolute;
+                top: 0;
+                right: 20px;
+                cursor: pointer;
+            }
+
+            .rotate {
+                transform: rotate(-45deg);
+                transition: all 1s ease-in;
+            }
+
+            #w-text{
+                position: absolute;
+                top:50%;
+                left: 55%;
+                width: 100%;
+                transform: translate(-50%,-50%);
+            }
+
+
+            #welcome h1{
+                font-size: 2.5em;
+
+            }
+
+            
+            }
+
+
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div id="wrapper">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                <div class="top-right">
+                    <h2>Sou Sou</h2>
+                   <p id="menu" onclick="menu()">Menu</p>
+                   <div id="link-container">
+                    <p id="close-btn" onclick="closeMenu(this)">+</p>
+                             @auth
+                     <a href="{{ url('/home') }}" class="links">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ route('login') }}" class="links">About</a>
+                    <a href="{{ route('login') }}" class="links">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}" class="links register">Register</a>
                         @endif
-                    @endauth
+                    @endauth  
+                   </div>
+            
+                  
                 </div>
+               
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <div id="welcome">
+                <div id="w-text">
+                    <h1>Welcome To Sou Sou</h1>
+                    <p>The online...........</p> 
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+               
+            </div>
+            <div id="how">
+                <h1>How it Works</h1>
+            </div>
+            <div class="content">
+                <div class="cards">
+                    <p>Join Wait List</p>
+                    <div>
+                    <img src="{{asset('dashboard/assets/img/join_list.svg')}}" alt="">
+                    </div>
+                    
+                </div>
+                <div class="cards">
+                    <p>Get Add to Group</p>
+                    <div>
+                    <img src="{{asset('dashboard/assets/img/admin.svg')}}" alt="">
+                    </div>
+                   
+                </div>
+                <div class="cards">
+                    <p>Bless Group Top User</p>
+                    <div>
+                    <img src="{{asset('dashboard/assets/img/bless.svg')}}" alt="">
+                    </div>
+                   
+                </div>
+                <div class="cards">
+                    <p>Continue Till you get to the Top</p>
+                    <div>
+                    <img src="{{asset('dashboard/assets/img/top.svg')}}" alt="">
+                    </div>
+                    
                 </div>
             </div>
         </div>
+    <script>
+        let btn = document.getElementById('menu');
+        function menu(){
+            let links = document.getElementById('link-container');
+            let close_btn = document.getElementById('close-btn');
+            close_btn.classList.add('rotate');
+            links.style.width = '60%';
+        }
+        function closeMenu(close_btn){
+            let links = document.getElementById('link-container');
+            links.style.width = '0';
+            close_btn.classList.remove('rotate');
+        }
+        
+    </script>
     </body>
+    
 </html>
